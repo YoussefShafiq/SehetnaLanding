22617b3d5fa2150d90c5de15def8c85094acfa8a
Youssef Shafek <youssefmsa616@gmail.com>
2025-04-28 16:09:56 +0300

    not found page update
    Signed-off-by: Youssef Shafek <youssefmsa616@gmail.com>

--- src/Components/Notfound.jsx
+++ src/Components/Notfound.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import notFoundAnimation from '../assets/images/404 error with a tired person-pana.svg'
+import { Helmet } from 'react-helmet'
+import Layout from './Layout'
+import { useNavigate } from 'react-router-dom'
 
+// Inside your Notfound component
+<Helmet>
+  <title>Page Not Found | Sehtnaa</title>
+  <meta name="robots" content="noindex" />
+  <meta name="description" content="The page you're looking for doesn't exist." />
+</Helmet>
 export default function Notfound() {
+  const navigate = useNavigate();
+
   return <>
-    <div className="grid h-[90vh]">
-      <img src={notFoundAnimation} className='w-1/3 m-auto' alt="page not found" />
-    </div>
+
+    <Layout >
+      <div className="h-[calc(100vh-125px)] flex flex-col items-center justify-center gap-3">
+        <h1 className='text-2xl font-bold'>Page Not Found | Sehtnaa</h1>
+        <button onClick={() => navigate('/')} className='bg-primary hover:bg-secondary transition-all text-white  py-1 px-2 rounded-xl' >Go home</button>
+      </div>
+    </Layout>
+
   </>
 }
